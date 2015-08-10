eec88c7bf959156492a081f79b95f984a8265071
dgcollier <david@gcollier.me>
2015-08-10 10:21:58 -0500

    updated to use methods and new parameters from Input.php to validate user input

--- parks_post.php
+++ parks_post.php
@@ -9,38 +9,78 @@ if (!empty($_POST)) {
     );
 
     try {
-        $nameInput = (Input::has('nameInput') ? Input::getString('nameInput') : null);
+        $nameInput = (Input::has('nameInput') ? Input::getString('nameInput', 4, 30) : null);
         $stmt->bindValue(':name', $nameInput, PDO::PARAM_STR);
-    } catch (Exception $e) {
+    } catch (OutOfRangeException $e) {
         $errors[] = 'Error on name field: ' . $e->getMessage();
-    }
+    } catch (InvalidArgumentException $e) {
+        $errors[] = 'Error on name field: ' . $e->getMessage();
+    } catch (DomainException $e) {
+        $errors[] = 'Error on name field: ' . $e->getMessage();
+    } catch (LengthException $e) {
+        $errors[] = 'Error on name field: ' . $e->getMessage();
+    } catch (Exception $e) {
+        $errors[] = 'Error occurred: ' . $e->getMessage();
+    } 
     
     try {
-        $locationInput = (Input::has('locationInput') ? Input::getString('locationInput') : null);
+        $locationInput = (Input::has('locationInput') ? Input::getString('locationInput', 2, 2) : null);
         $stmt->bindValue(':location', $locationInput, PDO::PARAM_STR);
-    } catch (Exception $e) {
+    } catch (OutOfRangeException $e) {
         $errors[] = 'Error on location field: ' . $e->getMessage();
-    }        
+    } catch (InvalidArgumentException $e) {
+        $errors[] = 'Error on location field: ' . $e->getMessage();
+    } catch (DomainException $e) {
+        $errors[] = 'Error on location field: ' . $e->getMessage();
+    } catch (LengthException $e) {
+        $errors[] = 'Error on location field: ' . $e->getMessage();
+    } catch (Exception $e) {
+        $errors[] = 'Error occurred: ' . $e->getMessage();
+    }       
 
     try {
-        $dateInput = (Input::has('dateInput') ? Input::getString('dateInput') : null);
+        $dateInput = (Input::has('dateInput') ? Input::getString('dateInput', 10, 10) : null);
         $stmt->bindValue(':dateEst', $dateInput, PDO::PARAM_STR);
-    } catch (Exception $e) {
+    } catch (OutOfRangeException $e) {
         $errors[] = 'Error on date field: ' . $e->getMessage();
-    }
-
-    try {
-        $areaInput = (Input::has('areaInput') ? Input::getNumber('areaInput') : null);
-        $stmt->bindValue(':area', $areaInput, PDO::PARAM_STR);
+    } catch (InvalidArgumentException $e) {
+        $errors[] = 'Error on date field: ' . $e->getMessage();
+    } catch (DomainException $e) {
+        $errors[] = 'Error on date field: ' . $e->getMessage();
+    } catch (LengthException $e) {
+        $errors[] = 'Error on date field: ' . $e->getMessage();
     } catch (Exception $e) {
-        $errors[] = 'Error on area field: ' . $e->getMessage();
+        $errors[] = 'Error occurred: ' . $e->getMessage();
     } 
 
     try {
-        $descrInput = (Input::has('descrInput') ? Input::getString('descrInput') : null);
+        $areaInput = (Input::has('areaInput') ? Input::getNumber('areaInput', 9000, 9000000) : null);
+        $stmt->bindValue(':area', $areaInput, PDO::PARAM_STR);
+    } catch (OutOfRangeException $e) {
+        $errors[] = 'Error on area field: ' . $e->getMessage();
+    } catch (InvalidArgumentException $e) {
+        $errors[] = 'Error on area field: ' . $e->getMessage();
+    } catch (DomainException $e) {
+        $errors[] = 'Error on area field: ' . $e->getMessage();
+    } catch (RangeException $e) {
+        $errors[] = 'Error on area field: ' . $e->getMessage();
+    } catch (Exception $e) {
+        $errors[] = 'Error occurred: ' . $e->getMessage();
+    } 
+
+    try {
+        $descrInput = (Input::has('descrInput') ? Input::getString('descrInput', 20, 500) : null);
         $stmt->bindValue(':descr', $descrInput, PDO::PARAM_STR);
-    }  catch (Exception $e) {
+    } catch (OutOfRangeException $e) {
         $errors[] = 'Error on description field: ' . $e->getMessage();
+    } catch (InvalidArgumentException $e) {
+        $errors[] = 'Error on description field: ' . $e->getMessage();
+    } catch (DomainException $e) {
+        $errors[] = 'Error on description field: ' . $e->getMessage();
+    } catch (LengthException $e) {
+        $errors[] = 'Error on description field: ' . $e->getMessage();
+    } catch (Exception $e) {
+        $errors[] = 'Error occurred: ' . $e->getMessage();
     } 
 
     if(empty($errors)) {
